72f2bcdc4f7f06cf0bf918dbb601c43ab4cbce71
Ray Gerrard <raygerrard@gmail.com>
2015-02-10 17:47:44 +0000

    added dropdown menu functionality and styling

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -5,6 +5,15 @@ angular.module('speechBubbleApp')
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
+    }, {
+      'title': 'Menu 1',
+      'link': '/menu',
+      'children': [
+        { 'title': 'link 1', 'link': '/menu/link1' },
+        { 'title': 'link 2', 'link': '/menu/link2' },
+        { 'title': 'link 3', 'link': '/menu/link3' },
+        { 'title': 'link 4', 'link': '/menu/link4' }
+      ]
     }];
 
     $scope.isLoggedIn = Auth.isLoggedIn;
