7b6ac497130fefbc405790deee6aed0558120fb9
Ray Gerrard <raygerrard@gmail.com>
2015-02-18 14:45:16 +0000

    added delete confirm functionality to admin

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('speechBubbleApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function ($scope, $http, Auth, User, Modal) {
 
     // Use the User $resource to fetch all users
     $scope.users = User.query();
     $scope.roles = ['admin', 'user'];
 
-    $scope.delete = function(user) {
+    $scope.delete = Modal.confirm.delete(function(user) { // callback when modal is confirmed
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
         if (u === user) {
           $scope.users.splice(i, 1);
         }
       });
-    };
+    });
 
     $scope.updateStatus = function(user) {
       User.updateStatus(user);
