43bddba9285b602e23670bd98d9f39f20722b6ca
Ray Gerrard <raygerrard@gmail.com>
2015-02-26 02:16:20 +0000

    removed admin restriction form image listing

--- server/api/upload/index.js
+++ server/api/upload/index.js
@@ -5,7 +5,7 @@ var controller = require('./upload.controller');
 var auth = require('../../auth/auth.service');
 var router = express.Router();
 
-router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/', controller.index);
 router.post('/', auth.hasRole('admin'), controller.create);
 router.delete('/:filename', auth.hasRole('admin'), controller.destroy);
 
