8b388ec438531244ad95079fd1fda866f59b3891
Ray Gerrard <raygerrard@gmail.com>
2015-02-26 02:27:27 +0000

    added $sce trust as html wrapper to flat-pages to fix issue with inline styles being stripped

--- client/app/page/page.controller.js
+++ client/app/page/page.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('speechBubbleApp')
-  .controller('PageCtrl', function ($scope, Page, $stateParams, $location) {
+  .controller('PageCtrl', function ($scope, Page, $stateParams, $location, $sce) {
     Page.get({ id: $stateParams.path.split('/').join('') }, function(res) {
-      $scope.content = res.content;
+      $scope.content = $sce.trustAsHtml(res.content);
       $scope.enableComments = res.enableComments;
     }, function() {
       $location.path('/404/');
