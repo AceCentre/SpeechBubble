be8a40c5af1fcd5c8d616dfcca705a7e7c7667ee
Ray Gerrard <raygerrard@gmail.com>
2015-03-04 04:12:55 +0000

    added supplier to Product schema

--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -4,7 +4,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-
+  supplier: {
+    type: Schema.Types.ObjectId;
+    ref: 'Supplier'
+  }
 }, { collection: 'products' });
 
 module.exports = {
