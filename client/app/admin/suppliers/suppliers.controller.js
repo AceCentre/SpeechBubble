69799b3576d8320a9ee97f6508ccc07a66cf9c1c
Ray Gerrard <raygerrard@gmail.com>
2015-03-04 03:18:28 +0000

    added client-side delete to supplier admin

--- client/app/admin/suppliers/suppliers.controller.js
+++ client/app/admin/suppliers/suppliers.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('speechBubbleApp')
-  .controller('AdminSupplierCtrl', function ($scope, $modal, Supplier) {
+  .controller('AdminSupplierCtrl', function ($scope, $modal, Modal, Supplier) {
     $scope.suppliers = Supplier.query();
     $scope.suppliersPerPage = 10;
     $scope.currentPage = 1;
@@ -29,5 +29,14 @@ angular.module('speechBubbleApp')
         }
       });
     };
+
+    $scope['delete'] = Modal.confirm['delete'](function(supplier) { // callback when modal is confirmed
+      Supplier.remove({ id: supplier._id });
+      angular.forEach($scope.suppliers, function(s, i) {
+        if (s === supplier) {
+          $scope.suppliers.splice(i, 1);
+        }
+      });
+    });
 
   });
