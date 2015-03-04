14ed91dc1bfbb88a6a4d92efd324f401f1efca65
Ray Gerrard <raygerrard@gmail.com>
2015-03-04 18:18:36 +0000

    allowed local development to use env var for db

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,10 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/speechbubble-dev'
+    uri: process.env.MONGOLAB_URI ||
+         process.env.MONGOHQ_URL ||
+         process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+         'mongodb://localhost/speechbubble'
   },
 
   seedDB: true
