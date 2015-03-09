c37291f224bb70f981109de3cd4fde96ce5a5abc
Ray Gerrard <raygerrard@gmail.com>
2015-03-09 10:22:32 +0000

    bug fixes

--- client/app/admin/products/products-destroy.directive.js
+++ client/app/admin/products/products-destroy.directive.js
@@ -4,8 +4,8 @@ angular.module('speechBubbleApp')
 .directive('destroy', function() {
   return function(scope, elem, attrs) {
     scope.$on('$destroy', function() {
-      if(scope.product[attrs.destroy]) {
-        delete scope.product[attrs.destroy];
+      if(scope.product.features[attrs.destroy]) {
+        delete scope.product.features[attrs.destroy];
       }
     });
   }
