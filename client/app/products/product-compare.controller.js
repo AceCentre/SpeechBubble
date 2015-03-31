eee7a06071cf817c99eb05bb8b123605b67fc71b
Ray Gerrard <raygerrard@gmail.com>
2015-03-31 16:12:15 +0100

    software compare view

--- client/app/products/product-compare.controller.js
+++ client/app/products/product-compare.controller.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('speechBubbleApp')
-.controller('ProductsCompareCtrl', function ($scope, $modalInstance, products) {
+.controller('ProductsCompareCtrl', function ($scope, $modalInstance, $sce, products) {
   $scope.products = products;
 
   $scope.cancel = function() {
     $modalInstance.dismiss();
+  };
+
+  $scope.getThumbnail = function(item) {
+    return $sce.trustAsResourceUrl( item.images.length && item.images[0].url || '/assets/images/products/default-thumbnail.png' );
   };
 
 });
