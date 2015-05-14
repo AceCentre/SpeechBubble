05983e1ca727577a7587f4bfbefcbaf9016f4b1a
Ray Gerrard <raygerrard@gmail.com>
2015-05-14 12:03:44 +0100

    fixed related software issue with hardware being displayed

--- server/api/product/product.controller.js
+++ server/api/product/product.controller.js
@@ -443,7 +443,7 @@ exports.addImages = function(req, res) {
 
 exports.getSoftwareForVocabulary = function(req, res) {
   console.log(typeof req.query.vocabulary);
-  Product.find({ 'features.premadeVocabulariesAvailable._id': req.query.vocabulary }, function(err, products) {
+  Product.find({ type: 'ProductSoftware', 'features.premadeVocabulariesAvailable._id': req.query.vocabulary }, function(err, products) {
     if(err) { return handleError(res, err); }
     return res.send(200, products);
   });
