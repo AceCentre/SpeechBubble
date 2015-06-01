b5b2cb1f7694cfe2b25aee017c8c4c7ebd5651b4
Ray Gerrard <raygerrard@gmail.com>
2015-06-01 20:39:43 +0100

    added new access solution product type for create, search, edit, detail and ratings

--- server/enum/index.js
+++ server/enum/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   REGION: ['', 'UK', 'Europe', 'USA', 'other'],
   USER_DESCRIPTION: ['', 'professional', 'parent', 'aac user', 'other'],
-  PRODUCT_TYPES: ['ProductHardware', 'ProductVocabulary', 'ProductSoftware', 'ProductLowTech'],
+  PRODUCT_TYPES: ['ProductHardware', 'ProductAccessSolution', 'ProductVocabulary', 'ProductSoftware', 'ProductLowTech'],
   REVISION_STATUS: ['Draft', 'Published', 'Removed']
 };
