045f5e30ba4eeb6571a1adda2245097d559193ca
Ray Gerrard <raygerrard@gmail.com>
2015-06-09 02:37:19 +0100

    new layout

--- client/components/header/header.controller.js
+++ client/components/header/header.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('speechBubbleApp')
-  .controller('HeaderCtrl', function ($scope, $location, Auth, ProductSearch, $state) {
-	  $scope.search = ProductSearch;
+  .controller('HeaderCtrl', function ($scope, $location, Auth, ProductSearch, $state, $timeout) {
+	  $scope.search = angular.copy(ProductSearch);
 	  
 	  $scope.submit = function(term, event) {
       var term = term || '';
@@ -16,7 +16,18 @@ angular.module('speechBubbleApp')
         return $location.url('/products');
       }
 		  return $location.url('/products').search({ 'term': term  })
-	  }
+	  };
+    
+    $scope.applyFilters = function() {
+      angular.copy($scope.search, ProductSearch);
+    };
+    
+    $scope.clearFilters = function() {
+      $state.go('products');
+      $timeout(function() {
+        $scope.search = angular.copy(ProductSearch);
+      });
+    };
     
     $scope.$watch('search.term', function(term) {
       $scope.term = term;
