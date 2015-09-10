763d9ffbe66e31d3190fd81ef4c8c64d19c4f71f
Raymond Gerrard <raygerrard@raymonds-mbp.home>
2015-09-10 20:22:50 +0100

    fixed issue with search sometimes not reloading

--- client/components/header/header.controller.js
+++ client/components/header/header.controller.js
@@ -3,14 +3,11 @@
 angular.module('speechBubbleApp')
   .controller('HeaderCtrl', function ($scope, $location, $http, Auth, ProductSearch, $state, $timeout) {
 	  $scope.search = angular.copy(ProductSearch);
-	  
+
 	  $scope.submit = function(term, event) {
       var term = term || '';
       if(event) {
         event.preventDefault();
-      }
-      if($state.current.name === 'products') {
-        return $scope.search.term = term;
       }
       if(!term) {
         $scope.applyFilters();
@@ -18,17 +15,17 @@ angular.module('speechBubbleApp')
       }
 		  return $location.url('/products').search({ 'term': term  })
 	  };
-    
+
     $scope.clearSearchRetainType = function() {
       var s = $scope.search;
       delete s.page;
       delete s.limit;
-      
+
       angular.forEach(s.facets, function(value, key) {
         delete s.facets[key];
       });
     };
-    
+
     $scope.getSimilar = function(term) {
       return $http.get('/api/product/similar', {
         params: {
@@ -38,16 +35,16 @@ angular.module('speechBubbleApp')
         return res.data;
       });
     };
-    
+
     $scope.applyFilters = function() {
       angular.copy($scope.search, ProductSearch);
     };
-    
+
     $scope.clearFilters = function() {
       $state.go('products');
       $timeout(function() {
         $scope.search = angular.copy(ProductSearch);
       });
     };
-    
+
   });
