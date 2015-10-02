5dcb17ebd67b32b9d80753f44b7fcc46b2f829c4
Raymond Gerrard <raygerrard@unknown.home>
2015-10-02 22:40:21 +0100

    filter null values in array

--- server/api/product/product.controller.js
+++ server/api/product/product.controller.js
@@ -7,10 +7,12 @@ var jade = require('jade');
 var Product = require('./product.model');
 var formidable = require('formidable');
 var mandrill = require('mandrill-api/mandrill');
+console.log(process.env.MANDRILL_API_KEY);
 var mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_API_KEY);
 var User = require('../user/user.model');
 
 function flatten(suppliers) {
+  suppliers = _.without(suppliers, null);
   return _.uniq(suppliers.map(function(supplier) {
     return _.isString(supplier) ? supplier: supplier._id;
   }));
