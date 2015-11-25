ff5bf0714ae69ec5bc3b787ecccfc63999d6161c
Will Wade <willwade@Wills-MacBook-Pro.local>
2015-11-25 22:07:46 +0000

    Revert "potential fix for the first item in compare issue"
    This reverts commit f3cd57c777ab2b67b1df2c3fbdce2727f4f6d8c2.

--- client/app/products/product-compare.controller.js
+++ client/app/products/product-compare.controller.js
@@ -12,14 +12,4 @@ angular.module('speechBubbleApp')
     return $sce.trustAsResourceUrl( item.images.length && item.images[0].url || '/assets/images/products/default-thumbnail.png' );
   };
 
-  $scope.productsContainCategory = function(products,category) {
-    var doesContain = false;
-    for (var index = 0; index < products.length; ++index) {
-        if (products[index].features.category === category){
-          doesContain = true;
-        }
-    }
-    return doesContain;
-  };
-
 });
